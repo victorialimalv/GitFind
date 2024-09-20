ad2c2eb02105f60443eac0099d39824ea905f444
Victoria Lima <victorialima@Air-de-Victoria.home>
2024-09-20 12:17:29 +0100

    add fetch request to api

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,33 @@
+import { useState } from 'react'; 
+
 import { Header } from "../../Components/Header";
 import background from '../../Assets/background.jpg';
 import './styles.css';
 import ItemList from "../../Components/ItemList";
 
 function App() {
+
+    const [user, setUser] = useState('');
+    const [currentUser, setCurrentUser] = useState(null);
+    const [repos, setRepos] = useState(null);
+
+    const handleGetData = async () => {
+        const userData = await fetch(`https://api.github.com/users/${user}`);
+        const newUser = await userData.json();
+
+        if(newUser.name) {
+            const {avatar_url, name, bio, login} = newUser;
+            setCurrentUser({avatar_url, name, bio, login});
+
+        const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+        const newRepos = await reposData.json();
+        
+        if(newRepos.length) {
+            setRepos(newRepos);
+        }
+    }
+};
+
     return (
         <div className="App">
             <Header />
@@ -11,32 +35,39 @@ function App() {
             <div className="background" style={{ backgroundImage: `url(${background})` }}>
 
                 <div className="info">
-                    <input placeholder="@username" name="usuario"/>
-                    <button>Buscar</button>
+                    <input placeholder="@username" name="usuario" value={user} 
+                    onChange={event => setUser(event.target.value)}/>
+                    <button onClick={handleGetData}>Buscar</button>
                 </div>
-
-                <div className="perfil">
-                <img className="profile" src="https://avatars.githubusercontent.com/u/133249763?v=4" alt="Perfil de Victoria Lima" />
-                    <div>
-                        <h3>Victoria Lima</h3>
-                        <span>@victorialimalv</span>
-                        <p>Descrição</p>
+                {currentUser?.name ? (
+                    <>
+                    <div className="perfil">
+                    <img 
+                    className="profile" 
+                    src={currentUser.avatar_url} 
+                    alt="Perfil de Victoria Lima" />
+                        <div>
+                            <h3>{currentUser.name}</h3>
+                            <span>@{currentUser.login}</span>
+                            <p>{currentUser.bio}</p>
+                        </div>
                     </div>
-                </div>
-                <hr />
+                    <hr />
+                    </>
+                ) : null}
 
-                <div>
-                    <h4 className="repositorio">Repositórios</h4>
-                    <ItemList title="teste1" description="teste de descrição 1"/>
-                    <ItemList title="teste1" description="teste de descrição 1"/>
-                    <ItemList title="teste1" description="teste de descrição 1"/>
-                    <ItemList title="teste1" description="teste de descrição 1"/>
-                </div>
-
-            </div>
-            </div>
-        </div>
-    );
+                {repos?.length ? (
+                     <div>
+                     <h4 className="repositorio">Repositórios</h4>
+                     {repos.map(repo => (
+                         <ItemList title={repo.name} description={repo.description}/>
+                        ))}
+                 </div> 
+                ) : null}
+             </div>
+             </div>
+         </div>
+     );   
 };
 
 export default App;
